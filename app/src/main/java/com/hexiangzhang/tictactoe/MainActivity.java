ef0672f30704ec9917a11b9564b978cfdecdfa8a
Hexiang Zhang <sam10.zhang@gmail.com>
2019-01-05 16:44:48 -0600

    Private var

--- app/src/main/java/com/hexiangzhang/tictactoe/MainActivity.java
+++ app/src/main/java/com/hexiangzhang/tictactoe/MainActivity.java
@@ -1,9 +1,23 @@
 package com.hexiangzhang.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean player1Turn = true;
+
+    private int roundCount;
+
+    private int player1Points;
+    private int private2Points;
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
